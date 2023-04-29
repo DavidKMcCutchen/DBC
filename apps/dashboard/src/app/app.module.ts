e51d706a31d162048e6e6c863cc9ddd631c739c4
David Kirtland McCutchen <davemccutch@gmail.com>
2023-04-29 18:04:23 -0400

    Initial Push

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -4,12 +4,21 @@ import { RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { appRoutes } from "./app.routes";
 import { NxWelcomeComponent } from "./nx-welcome.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+
+
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: "enabledBlocking" }),
+    ButtonModule,
+    RippleModule
+
   ],
   providers: [],
   bootstrap: [AppComponent],
